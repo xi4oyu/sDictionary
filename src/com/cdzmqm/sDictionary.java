c2a16555c328f89964640e8d0e269d1d329db089
Chora <Chora@Chora-PC>
2015-07-02 12:20:28 +0800

    Second

--- src/com/cdzmqm/sDictionary.java
+++ src/com/cdzmqm/sDictionary.java
@@ -1,5 +1,14 @@
 package com.cdzmqm;
 
-class sDictionary {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+class sDictionary {
+	public static void main(String[] args) {
+		Matcher tmp = Pattern.compile("([0-9]{4})([0-9]{4})").matcher("19911104");
+		if(tmp.find())
+		{
+			System.out.println(tmp.group());
+		}
+	}
 }
